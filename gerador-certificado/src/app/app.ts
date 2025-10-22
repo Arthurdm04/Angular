901f1ce1fef46009937e5e141ce079cde89646e4
Arthur Dias Moresco <morescoarthur@gmail.com>
2025-10-22 16:10:14 -0300

    Componente criado base-ui

--- gerador-certificado/src/app/app.ts
+++ gerador-certificado/src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUI } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUI],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
